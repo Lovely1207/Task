7cc075c5e589a0292916dc87f7a07d6a83d3172a
Lovely1207 <48162650+Lovely1207@users.noreply.github.com>
2020-08-24 01:27:21 +0530

    Update NumberRepository.java

--- NumberRepository.java
+++ NumberRepository.java
@@ -1,8 +1,16 @@
 package com.glaucus.Counter;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface NumberRepository extends JpaRepository<Number, Long> {
-
+public interface NumberRepository extends JpaRepository<Number,Integer> {
 	
+	
+	 @Query("SELECT n from Number n where n.count =:count ")       // using @query
+	 List<Number> findByCount(@Param("count") Integer count);
 }
